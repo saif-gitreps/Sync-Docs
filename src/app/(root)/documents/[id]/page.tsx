a7be5aff1c8ee5848837bd8e9d1686f2d9f4f4df
Saif <droconos99@gmail.com>
2024-11-11 23:34:01 +0600

    Add: Auth forms and middelwares using Clerk auth service.

--- src/app/(root)/documents/[id]/page.tsx
+++ src/app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,22 @@
 import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 function Document() {
    return (
       <div>
+         <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+               <div className="document-title">Share</div>
+            </div>
+            <SignedOut>
+               <SignInButton />
+            </SignedOut>
+            <SignedIn>
+               <UserButton />
+            </SignedIn>
+         </Header>
          <Editor />
       </div>
    );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const geistSans = localFont({
    src: "./fonts/GeistVF.woff",
@@ -24,10 +26,20 @@ export default function RootLayout({
    children: React.ReactNode;
 }>) {
    return (
-      <html lang="en" suppressHydrationWarning>
-         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-            {children}
-         </body>
-      </html>
+      <ClerkProvider
+         appearance={{
+            baseTheme: dark,
+            variables: {
+               colorPrimary: "#3371FF",
+               fontSize: "16px",
+            },
+         }}
+      >
+         <html lang="en" suppressHydrationWarning>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+               {children}
+            </body>
+         </html>
+      </ClerkProvider>
    );
 }
